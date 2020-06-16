fd968bbae95fc9ba4169502320411b2f2da997f1
bp mike <66115661+bp-mike@users.noreply.github.com>
2020-06-16 15:05:41 +0300

    Add files via upload

---
+++ bp_mike.js
@@ -0,0 +1,50 @@
+
+var name; // declaration  / creating a varible calling it name
+
+name = " ozzy queen" //initialisation /assigning the variable name to give a string value ozzy queen
+
+console.log(name); // printing the value of the variable name in the console
+
+console.log("<br/>");//breaking the line and jumping to the next line in the console
+
+var name;// declaration  / creating a varible calling it name
+
+name = "ozzy hum"//initialisation /assigning the variable name to give a string value ozzy hum
+
+console.log(name)//printing the value of the variable name in the console
+
+var name; // declaration  / creating a varible calling it name
+
+name = "sumi mike ozyy" //initialisation /assigning the variable name to give a string value sumi mike ozyy
+
+console.log(name)//printing the value of the variable name in the console
+
+var name;// declaration  / creating a varible calling it name
+
+name = " ozzy bp"//initialisation /assigning the variable name to give a string value ozzy bp
+
+console.log(name)//printing the value of the variable name in the console
+
+name = " Bridget ozzy"//initialisation /assigning the variable name to give a string value bridget ozzy 
+
+console.log(name)//printing the value of the variable name in the console
+
+var age;// declaration  / creating a varible calling it age
+
+var name;// declaration  / creating a varible calling it name
+
+age = 50;//initialisation /assigning the variable age an int value 50
+name = "mirembe agaba kwesi"//initialisation /assigning the variable name to give a string value mirembe agaba kwesi
+
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");/* print 'my name is' and get value of variable name and print it
+then print a space , followed by and 'i am' , then get and print value of varible age , then space, then 'years old'
+*/
+var x;// declaration  / creating a varible calling it x
+
+document.write(x);//print variable x
+
+var x = undefined; //declaring variable x and initialisising an undefined value to it
+
+console.log(x); //printing the value of the variable x in the console, this returns nothing because the value is undefined datatype
+
+
